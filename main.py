9fde18d9bf602aa72e9d0eefae25f562abca703b
Cube219 <max804800@gmail.com>
2020-08-11 20:20:02 +0900

    🏗 Collect codes into MelonCrawler class

--- main.py
+++ main.py
@@ -5,47 +5,57 @@ import os
 from dotenv import load_dotenv
 import pprint
 
-def GetSongInfos() :
-    melonPageReq = requests.get("https://www.melon.com/chart/index.htm", headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"})
-    if melonPageReq.ok == False:
-        print(f"Failed to get a Melon chart page. (Status Code: {melonPageReq.status_code})")
-        return []
+class MelonCrawler :
+    ytmusic: YTMusic
+    playlist_name = ""
+    melon_song_infos = []
 
-    soup = BeautifulSoup(melonPageReq.content, "html.parser")
-    songInfos = soup.select(".wrap_song_info")
+    def init_youtube_music_api(self, cookie, playlist_name) :
+        if cookie == None :
+            print("Cookie is not set.")
+            return
+        
+        json_str = """{
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Content-Type": "application/json",
+            "X-Goog-AuthUser": "0",
+            "x-origin": "https://music.youtube.com",
+            "Cookie" : "%s"
+        }""" % cookie
 
-    songs = []
-    for i in range(0, len(songInfos), 2) :
-        tmp = songInfos[i].find_all("div")
+        self.ytmusic = YTMusic(json_str)
+        self.playlist_name = playlist_name
 
-        title = tmp[0].span.a.text
-        artist = tmp[1].span.a.text
-        album = songInfos[i + 1].div.a.text
+    def update_song_infos(self) :
+        melon_page_req = requests.get("https://www.melon.com/chart/index.htm", headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"})
+        if melon_page_req.ok == False:
+            print(f"Failed to get a Melon chart page. (Status Code: {melon_page_req.status_code})")
+            return
 
-        songs.append((title, artist, album))
+        soup = BeautifulSoup(melon_page_req.content, "html.parser")
+        song_infos = soup.select(".wrap_song_info")
 
-    return songs
+        self.melon_song_infos = []
+        for i in range(0, len(song_infos), 2) :
+            tmp = song_infos[i].find_all("div")
+
+            title = tmp[0].span.a.text
+            artist = tmp[1].span.a.text
+            album = song_infos[i + 1].div.a.text
+
+            self.melon_song_infos.append((title, artist, album))
 
 # ----------------------------------------------
 
 load_dotenv(verbose=True)
 
 cookie = os.getenv("COOKIE")
+playlist_name = os.getenv("PLAYLIST_NAME")
+if playlist_name == None :
+    playlist_name = "Melon Chart 100"
 
-if cookie == None :
-    print("Cookie is not set.")
-    # return
-
-jsonStr = """{
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
-    "Accept": "*/*",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Content-Type": "application/json",
-    "X-Goog-AuthUser": "0",
-    "x-origin": "https://music.youtube.com",
-    "Cookie" : "%s"
-}""" % cookie
-
-ytmusic = YTMusic(jsonStr)
-
-search_results = ytmusic.get_library_playlists()
+crawler = MelonCrawler()
+crawler.init_youtube_music_api(cookie, playlist_name)
+crawler.update_song_infos()
